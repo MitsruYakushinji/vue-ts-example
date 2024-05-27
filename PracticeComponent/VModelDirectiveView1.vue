357b45535020bdcb778b74ffec184bd0563e7495
MitsruYakushinji <156178833+MitsruYakushinji@users.noreply.github.com>
2024-05-27 10:12:40 +0900

    Chapter2完了

---
+++ PracticeComponent/VModelDirectiveView1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- inputの数値を合算する処理の追加 -->
+    <input type="number" v-model="num1" />
+    <input type="number" v-model="num2" />
+    <!-- 合算した値の表示 -->
+    <p>{{ sum() }}</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    // inputの数値を保持する変数の追加
+    const num1 = ref(0);
+    const num2 = ref(0);
+
+    const sum = () => {
+      // 2つのinputの数値を合算する処理の追加
+      return Number(num1.value) + Number(num2.value);
+    };
+
+    return {
+      num1,
+      num2,
+      sum,
+    };
+  },
+});
+</script>
---
+++ PracticeComponent/VModelDirectiveView2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label for="baseball">野球</label>
+    <input id="baseball" type="checkbox" value="baseball" v-model="sports" />
+    <label for="football">サッカー</label>
+    <input id="football" type="checkbox" value="football" v-model="sports" />
+    <div>
+      {{ sports }}
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const sports = ref(["football"]);
+
+    return {
+      sports,
+    };
+  },
+});
+</script>
